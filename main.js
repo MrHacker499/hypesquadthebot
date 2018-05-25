4cf845eb69a532a46194121f0429158948b1ea79
MrHacker499 <37866538+MrHacker499@users.noreply.github.com>
2018-05-24 20:24:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ if (err) console.error(err)
 });
 
 bot.on("guildCreate", guild => {
-	bot.user.setGame(`Type pr!help for help! | in ${bot.guilds.size} servers! | Square & ThatMajesticGuy`);
+	bot.user.setGame(`Type !help for help! | in ${bot.guilds.size} servers! | v1.10`);
   const channel = guild.channels.find('name', 'general');
   var embed = new Discord.RichEmbed()
   .setTitle("Hello! :wave:")
